c0d31027e217dbfb623b55f1dc5c53413e98c3e2
johnMalagon <johnalexandermalagongarcia@gmail.com>
2023-10-11 21:42:11 -0500

    ejercicio1

--- 01.js
+++ 01.js
@@ -11,6 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  var array2 = [];
+  for (var i = 0; i < array.length; i++) {
+    if (typeof array[i] === 'number') {
+      array2.push(array[i]);
+    }
+  }
+  return array2
 
 }
 
